96fedecc1805942c14077eae6b091131544ec202
JoshuaZe <joshua_zhang@foxmail.com>
2015-03-10 14:15:21 +0800

    basic idea

---
+++ TopicDiscovery.R
@@ -0,0 +1,8 @@
+#####
+# how to define a topic？
+# how can u make sure that u find a topic?
+# do these so called topics exist?
+# definition one : a topic is a bag of keywords with probabilistic
+# definition two : a topic is a stable/Widely accepted structure of keywords.
+#####
+#
