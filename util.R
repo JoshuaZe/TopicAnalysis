fa6015f0f376f3287dd8978c062accac08c61072
JoshuaZe <joshua_zhang@foxmail.com>
2015-03-10 10:58:26 +0800

    util.R

---
+++ util.R
@@ -0,0 +1,26 @@
+memoryWhiteList <- c("memoryWhiteList","convertDimensionToAttribute","rmTempObject","addPersistentObjects")
+addPersistentObjects <- function(names){
+  memoryWhiteList <<- unique(c(memoryWhiteList,names))
+}
+rmTempObject <- function(){
+  rm(list = setdiff(ls(envir = globalenv()), memoryWhiteList),envir = globalenv())
+  gc()
+  #ls(envir = globalenv())
+}
+
+library(plyr)
+convertDimensionToAttribute<-function(data,dimensions,values,DIMENSION_NAME="ATTRIBUTE",VALUE_NAME="VALUE"){
+  head(data$UNIQUE<-c(1:nrow(data)),1)
+  head(rownames(data)<-c(1:nrow(data)),1)
+  head(AttributeWithValue<-as.data.frame(
+    as.table(
+      as.matrix(
+        data[,values]
+      ))),5)
+  head(colnames(AttributeWithValue)<-c("UNIQUE",DIMENSION_NAME,VALUE_NAME),1)
+  #library(plyr)
+  system.time(result <- join(data[,append(dimensions,ncol(data))], AttributeWithValue, by = c("UNIQUE")))
+  result$UNIQUE<-NULL
+  head(result)
+  return(result)
+}
